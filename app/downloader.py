5e6af4717406eed8904c20564f7bc67fe28ef3e6
ZehNaemosports <zeh@naemosports.com>
2025-08-09 12:49:44 +0100

    added veo download

--- app/downloader.py
+++ app/downloader.py
@@ -11,6 +11,35 @@ class YoutubeDownloader:
         self.quality = quality
 
     async def download(self, url: str, filename: str) -> Optional[str]:
+        if "app.veo.co" in url:
+            try:
+                subprocess.run(["yt-dlp", "--version"], check=True, capture_output=True)
+                output_path = f"{filename}.mp4"
+        
+                cmd = [
+                    "yt-dlp",
+                    "-o", output_path,
+                    "--merge-output-format", "mp4",
+                    "--no-check-certificate",
+                    url
+                ]
+                
+                result = subprocess.run(cmd, check=True, capture_output=True, text=True)
+                print("Download completed successfully!")
+                final_filename = f'{filename}.mp4'
+                if Path(final_filename).exists():
+                    return Path(final_filename).absolute()
+                else:
+                    print("Download failed:", result.stderr)
+                    return None
+                
+            except subprocess.CalledProcessError as e:
+                print(f"Error downloading video: {e.stderr}")
+                return None
+            except FileNotFoundError:
+                print("yt-dlp not found. Please install it first.")
+                return None
+            
         format_selector = f'bestvideo[height<={self.quality}]+bestaudio/best[height<={self.quality}]'
 
         cmd = [
@@ -41,3 +70,6 @@ class YoutubeDownloader:
         except FileNotFoundError:
             print("Error: yt-dlp not found. Please install yt-dlp first.")
             return None
+        
+# s = YoutubeDownloader()
+# print(s.download(url="https://app.veo.co/matches/20250727-ivory-coast-contre-makinde-fc-vs-kolia-fc-ligafci-a2d2c313/", filename="test"))
