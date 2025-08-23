235aac24ee800e9e667693df9c64e100f2837309
ZehNaemosports <zeh@naemosports.com>
2025-08-23 08:16:02 +0100

    fix

--- app/downloader.py
+++ app/downloader.py
@@ -8,7 +8,7 @@ import logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 class YoutubeDownloader:
-    def __init__(self, quality='1080', cookies_path: Optional[str] = "/home/ubuntu/cookies.txt", use_tor=True):
+    def __init__(self, quality='1080', cookies_path: Optional[str] = "/home/ubuntu/cookies.txt", use_tor=False):
         self.quality = quality
         self.cookies_path = cookies_path
         self.use_tor = use_tor
