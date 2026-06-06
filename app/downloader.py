b7e2a54be3e6286e3acd070655db619f79550fa7
Zeh237 <zehbrien@gmail.com>
2026-06-06 12:03:49 +0100

    updates on file support

--- app/downloader.py
+++ app/downloader.py
@@ -337,7 +337,7 @@ class YoutubeDownloader:
             cmd.extend([
 
                 "--proxy",
-                "socks5://127.0.0.1:9050",
+                "socks5h://127.0.0.1:9050",
 
                 "--concurrent-fragments",
                 "1",
